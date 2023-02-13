745c88c39c725525b12b4f17347fd1d6ffac6b02
grzeli <grzelak.szymon@gmail.com>
2023-02-13 11:36:25 +0100

    added tailwind && configured eslint

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,9 +2,9 @@ export default function Head() {
   return (
     <>
       <title>IMDB</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="IMDB aggregator" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='IMDB aggregator' />
+      <link rel='icon' href='/favicon.ico' />
     </>
-  )
+  );
 }
