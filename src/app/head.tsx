f86eb362008f62f1ecc41727ecd1a31a11a0fdba
grzeli <grzelak.szymon@gmail.com>
2023-02-13 10:38:11 +0100

    installed next.js

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDB</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="IMDB aggregator" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
